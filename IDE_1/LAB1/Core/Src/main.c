5db5747a635930e865ca63f25d506f71f14a4e22
Huy Do <huy.do2607@hcmut.edu.vn>
2025-09-22 03:41:29 +0700

    Update main.c

--- IDE_1/LAB1/Core/Src/main.c
+++ IDE_1/LAB1/Core/Src/main.c
@@ -54,12 +54,16 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+uint16_t led[12] = {
+    GPIO_PIN_1, GPIO_PIN_2, GPIO_PIN_3, GPIO_PIN_4,
+    GPIO_PIN_5, GPIO_PIN_6, GPIO_PIN_7, GPIO_PIN_8,
+    GPIO_PIN_9, GPIO_PIN_10, GPIO_PIN_11, GPIO_PIN_12
+};
+
 void clearAllClock(void) {
-    HAL_GPIO_WritePin(GPIOA,
-        GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|
-        GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11|
-        GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15,
-        GPIO_PIN_SET);  // SET = tắt LED
+    for (int i = 0; i < 12; i++) {
+        HAL_GPIO_WritePin(GPIOA, led[i], GPIO_PIN_SET); // SET = 1 = tắt LED
+    }
 }
 /* USER CODE END 0 */
 
